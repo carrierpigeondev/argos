feb7f755aeaba6c71d2b0aaf63347121a72e100a
Carrier Pigeon <132307322+carrierpigeondev@users.noreply.github.com>
2025-12-02 14:20:43 -0600

    fixed entrypoint / custom-setup script

--- Assets/StreamingAssets/build.bat
+++ Assets/StreamingAssets/build.bat
@@ -1,2 +1,2 @@
-.\RedHat\Podman\podman.exe build -f .\Containerfile -t debian-argos:latest .
+.\RedHat\Podman\podman.exe build -f .\Containerfile --no-cache -t debian-argos:latest .
 .\RedHat\Podman\podman.exe save --format oci-archive -o .\debian-argos.oci.tar debian-argos:latest
